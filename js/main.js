951d52a6b98e8cd96a2655e55b2978e6ef225e59
jeancharlesgraillot <graillot.jc@gmail.com>
2018-11-05 15:58:43 +0100

    part of html done

---
+++ js/main.js
@@ -0,0 +1,12 @@
+// Exo 3
+
+// un objet pour créer vos produits
+// • un objet pour gérer votre formulaire(récupérer les données des inputs, créer un produits et
+// l'insérer dans le DOM)
+
+
+function Product(title, catchphrase, description) {
+  this.title = title;
+  this.catchphrase = catchphrase;
+  this.description = description;
+}
